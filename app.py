c18d866aafa4ffbd63d23cdf1f3902c31847afea
Anthuan18 <anthuanromanprieto@gmail.com>
2026-07-10 12:49:22 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -739,8 +739,8 @@ elif st.session_state.vista_actual == 'curso':
                 fig_disc_mat.add_trace(go.Scatter(x=ff, y=dd, mode='lines+markers', name=f'{SIMBOLOS_CURSOS[m]} {m}',
                     line=dict(color=COLORES_MATERIAS[i], width=2),
                     marker=dict(size=6),
-                    hovertemplate='<b>%{x|%Y-%m-%d}</b><br>Disciplina: %{y:.1f}%<br>Horas: %{customdata[0]}h<br>%{customdata[1]}<extra></extra>',
-                    customdata=list(zip(hh, dias_no_registrados))))         
+                    hovertemplate='<b>%{x|%Y-%m-%d}</b><br>Disciplina: %{y:.1f}%<br>Horas: %{customdata}h<extra></extra>',
+                    customdata=hh))       
          
         fig_disc_mat.update_layout(yaxis_title='Disciplina (%)', yaxis=dict(range=[0, 150]), xaxis=dict(tickformat='%Y-%m-%d', tickangle=45), hovermode='x unified', height=500, margin=dict(l=50, r=20, t=20, b=50))
         st.plotly_chart(fig_disc_mat, use_container_width=True)
