8a77a5210037abbafc20bbc215eb3a7c76c39aca
Anthuan18 <anthuanromanprieto@gmail.com>
2026-07-10 11:47:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -609,8 +609,8 @@ if st.session_state.vista_actual == 'general':
             fig_vel.add_trace(go.Scatter(x=fechas, y=vel_prom, mode='lines+markers', name='Velocidad', 
                 line=dict(color='gold', width=3), 
                 marker=dict(size=8, color='gold'), 
-                hovertemplate='<b>%{x|%Y-%m-%d}</b><br>Velocidad: %{y:.1f} ejer/h<br>-Resolviste %{customdata[0]} ejer en %{customdata[1]}h<extra></extra>',
-                customdata=list(zip(ejercicios_prom, horas_prom))))
+                hovertemplate='<b>%{x|%Y-%m-%d}</b><br>Velocidad: %{y:.1f} ejer/h<br>-Resolviste %{customdata[0]} ejer en %{customdata[1]}h<br>%{customdata[2]}<extra></extra>',
+                customdata=list(zip(ejercicios_prom, horas_prom, materias_str_prom))))
             fig_vel.update_layout(yaxis_title='Velocidad (ejercicios/h)', yaxis=dict(range=[0, max(30, max(vel_prom)*1.2)]), xaxis=dict(tickformat='%Y-%m-%d', tickangle=45), hovermode='x unified', height=400, margin=dict(l=50, r=20, t=20, b=50))
             st.plotly_chart(fig_vel, use_container_width=True)
         else:
