0545974591753a4133fa59d8729fd48c1092e2ac
Anthuan18 <anthuanromanprieto@gmail.com>
2026-07-12 11:10:42 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -553,7 +553,7 @@ if st.session_state.vista_actual == 'general':
         fig_disc.add_trace(go.Scatter(x=fechas, y=disc_prom, mode='lines+markers', name='Disciplina', 
             line=dict(color='#FF4500', width=3), 
             marker=dict(size=8, color='#FF4500'), 
-            hovertemplate='<b>%{x|%Y-%m-%d}</b><br>🔥 %{y:.1f}% 🔥<br>%{customdata[0]}h de<br>%{customdata[1]}<extra></extra>',
+            hovertemplate='<b>%{x|%Y-%m-%d}</b><br>🔥 %{y:.1f}% 🔥<br>%{customdata[0]}h de +<br>%{customdata[1]}<extra></extra>',
             customdata=list(zip(horas_prom, materias_str_prom))))        
         fig_disc.update_layout(yaxis_title='Disciplina (%)', yaxis=dict(range=[0, 150]), xaxis=dict(tickformat='%Y-%m-%d', tickangle=45), hovermode='closest', height=400, margin=dict(l=50, r=20, t=20, b=50))
         st.plotly_chart(fig_disc, use_container_width=True)
@@ -566,7 +566,7 @@ if st.session_state.vista_actual == 'general':
             fig_vel.add_trace(go.Scatter(x=fechas, y=vel_prom, mode='lines+markers', name='Velocidad', 
                 line=dict(color='gold', width=3),
                 marker=dict(size=8, color='gold'),
-                hovertemplate='<b>%{x|%Y-%m-%d}</b><br>⚡%{y:.1f}ejer/h⚡ <br> %{customdata[0]} ejer en %{customdata[1]}h<br> de %{customdata[2]}<extra></extra>',
+                hovertemplate='<b>%{x|%Y-%m-%d}</b><br>⚡%{y:.1f}ejer/h⚡ <br> %{customdata[0]} ejer en %{customdata[1]}h<br> de +%{customdata[2]}<extra></extra>',
                 customdata=list(zip(ejercicios_prom, horas_prom, materias_str_prom))))
             fig_vel.update_layout(yaxis_title='Velocidad (ejer/h)', yaxis=dict(range=[0, max(30, max(vel_prom)*1.2)]), xaxis=dict(tickformat='%Y-%m-%d', tickangle=45), hovermode='closest', height=400, margin=dict(l=50, r=20, t=20, b=50))
             st.plotly_chart(fig_vel, use_container_width=True)
